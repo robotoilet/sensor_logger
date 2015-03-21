40506e7107abfd77a8af79772b17e58ad236827f
Alecs Geuder <al.geuder@gmail.com>
2015-03-21 12:40:31 +0000

    outlier cleaning for HC_SR04

--- sensors/hc_sr04.py
+++ sensors/hc_sr04.py
@@ -1,35 +1,56 @@
-def setup():
+import math
+import time
+
+TRIG, ECHO = 17, 18
+
+def setup(trigger=TRIG, echo=ECHO):
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
 
-    TRIG, ECHO = 17, 18
+    GPIO.setup(trigger,GPIO.OUT)
+    GPIO.setup(echo,GPIO.IN)
+    GPIO.output(trigger, False)
+    print("Waiting For Sensor To Settle")
+    time.sleep(2)
 
-    def sense(trigger=TRIG, echo=ECHO):
+    def sense():
         """
         Sonar distance sensor
         """
-        GPIO.setup(TRIG,GPIO.OUT)
-        GPIO.setup(ECHO,GPIO.IN)
-        
-        GPIO.output(TRIG, False)
-        print("Waiting For Sensor To Settle")
-        time.sleep(2)
-        
-        GPIO.output(TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG, False)
-        
-        while GPIO.input(ECHO) == 0:
-          pulse_start = time.time()
-        
-        while GPIO.input(ECHO) == 1:
-          pulse_end = time.time()      
-        
+
+        GPIO.output(trigger, True)
+        time.sleep(0.0001)
+        GPIO.output(trigger, False)
+
+        while GPIO.input(echo) == 0:
+            pulse_start = time.time()
+
+        while GPIO.input(echo) == 1:
+            pulse_end = time.time()
+
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 17150
-        distance = round(distance, 2)
-        
-        print("Distance:",distance,"cm")
-        
-        GPIO.cleanup()
-    return sense
+        GPIO.output(trigger, False)
+        return round(distance, 2)
+
+        # TODO: do we need this???
+        ##GPIO.cleanup()
+
+    def outliers_cleaned():
+        """
+        Remove outliers, which are defined as having a distance from the mean
+        of five measurments bigger than two standard deviations.
+
+        Returns the mean of the remaining measurments.
+        """
+        measurments = [sense() for i in range(5)]
+        print("measurments to be outlier cleaned: %s" % measurments)
+
+        mean = lambda values: sum(values) / len(values)
+        avg = mean(measurments)
+        variance = map(lambda x: (x - avg) ** 2, measurments)
+        stdev = math.sqrt(mean(variance))
+        measurments = [m for m in measurments if abs(m - avg) <= stdev]
+        return mean(measurments)
+
+    return outliers_cleaned
